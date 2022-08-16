81c379131997ca9d3428591f875e0f162a5d76fc
LeeSeungjae00 <seungjae266789@gmail.com>
2022-08-16 13:43:53 +0900

    [fix] subgrop, antiviral UI 수정

--- src/Content/ResultTable.jsx
+++ src/Content/ResultTable.jsx
@@ -13,9 +13,9 @@ const useStyles = makeStyles((theme) => ({
         color: "blue",
         alignSelf: "center",
     },
-    tbCell : {
-        padding : "3px 0px",
-        fontSize : '0.87rem'
+    tbCell: {
+        padding: "3px 0px",
+        fontSize: '0.87rem'
     }
 }));
 
@@ -23,7 +23,7 @@ const useStyles = makeStyles((theme) => ({
 export default function ResultTable({ rows, loading, pill, rows2, subgroub }) {
     const classes = useStyles();
 
-    
+
 
     const options = {
         legend: {
@@ -43,12 +43,12 @@ export default function ResultTable({ rows, loading, pill, rows2, subgroub }) {
                         rows[0].val4, rows[0].val5, rows[0].val6,
                         rows[0].val7, rows[0].val8, rows[0].val9,
                         rows[0].val10
-                    )+5,
-                    stepSize : 1
+                    ) + 5,
+                    stepSize: 1
                 }
             }]
         },
-        defaultFontSize : 14,
+        defaultFontSize: 14,
         maintainAspectRatio: false // false로 설정 시 사용자 정의 크기에 따라 그래프 크기가 결정됨.
     }
 
@@ -70,25 +70,25 @@ export default function ResultTable({ rows, loading, pill, rows2, subgroub }) {
                         rows2[0].val4, rows2[0].val5, rows2[0].val6,
                         rows2[0].val7, rows2[0].val8, rows2[0].val9,
                         rows2[0].val10
-                    )+5,
-                    stepSize : 1
+                    ) + 5,
+                    stepSize: 1
                 }
             }]
         },
-        defaultFontSize : 14,
+        defaultFontSize: 14,
         maintainAspectRatio: false // false로 설정 시 사용자 정의 크기에 따라 그래프 크기가 결정됨.
     }
-    
+
 
     const data = {
-        labels: ['1yr', '2yr', '3yr' , '4yr', '5yr' ,'6yr','7yr','8yr','9yr','10yr'],
+        labels: ['1yr', '2yr', '3yr', '4yr', '5yr', '6yr', '7yr', '8yr', '9yr', '10yr'],
         legend: {
             display: false, // label 숨기기
         },
-        
+
         datasets: [
             {
-                
+
                 label: "",
                 type: 'line',
                 legend: false,
@@ -121,14 +121,14 @@ export default function ResultTable({ rows, loading, pill, rows2, subgroub }) {
 
 
     const data2 = {
-        labels: ['1yr', '2yr', '3yr' , '4yr', '5yr' ,'6yr','7yr','8yr','9yr','10yr'],
+        labels: ['1yr', '2yr', '3yr', '4yr', '5yr', '6yr', '7yr', '8yr', '9yr', '10yr'],
         legend: {
             display: false, // label 숨기기
         },
-        
+
         datasets: [
             {
-                
+
                 label: "",
                 type: 'line',
                 legend: false,
@@ -164,7 +164,7 @@ export default function ResultTable({ rows, loading, pill, rows2, subgroub }) {
             <h4 className="graphTitle">AI-predicted HCC risk under TDF treatment</h4>
             <hr className="resultLine"></hr>
             <TableContainer component={Paper}>
-                <Table className={classes.table} aria-label="simple table" size = "small">
+                <Table className={classes.table} aria-label="simple table" size="small">
                     {loading ?
                         <div className="tableLoding-rappeer" style={{
                             height: 250
@@ -174,43 +174,43 @@ export default function ResultTable({ rows, loading, pill, rows2, subgroub }) {
                         <>
                             <TableHead>
                                 <TableRow>
-                                    <TableCell className = {classes.tbCell} align="right">Year(s)</TableCell>
-                                    <TableCell className = {classes.tbCell} align="right">1</TableCell>
-                                    <TableCell className = {classes.tbCell} align="right">2</TableCell>
-                                    <TableCell className = {classes.tbCell} align="right">3</TableCell>
-                                    <TableCell className = {classes.tbCell} align="right">4</TableCell>
-                                    <TableCell className = {classes.tbCell} align="right">5</TableCell>
-                                    <TableCell className = {classes.tbCell} align="right">6</TableCell>
-                                    <TableCell className = {classes.tbCell} align="right">7</TableCell>
-                                    <TableCell className = {classes.tbCell} align="right">8</TableCell>
-                                    <TableCell className = {classes.tbCell} align="right">9</TableCell>
-                                    <TableCell className = {classes.tbCell} align="right">10</TableCell>
+                                    <TableCell className={classes.tbCell} align="right">Year(s)</TableCell>
+                                    <TableCell className={classes.tbCell} align="right">1</TableCell>
+                                    <TableCell className={classes.tbCell} align="right">2</TableCell>
+                                    <TableCell className={classes.tbCell} align="right">3</TableCell>
+                                    <TableCell className={classes.tbCell} align="right">4</TableCell>
+                                    <TableCell className={classes.tbCell} align="right">5</TableCell>
+                                    <TableCell className={classes.tbCell} align="right">6</TableCell>
+                                    <TableCell className={classes.tbCell} align="right">7</TableCell>
+                                    <TableCell className={classes.tbCell} align="right">8</TableCell>
+                                    <TableCell className={classes.tbCell} align="right">9</TableCell>
+                                    <TableCell className={classes.tbCell} align="right">10</TableCell>
                                 </TableRow>
                             </TableHead>
                             <TableBody>
                                 {
                                     rows.map((row) => (
-                                        <TableRow key = {row.val1}>
-                                            <TableCell className = {classes.tbCell} align="right"></TableCell>
-                                            <TableCell className = {classes.tbCell} align="right">{row.val1 + "%"}</TableCell>
-                                            <TableCell className = {classes.tbCell} align="right">{row.val2 + "%"}</TableCell>
-                                            <TableCell className = {classes.tbCell} align="right">{row.val3 + "%"}</TableCell>
-                                            <TableCell className = {classes.tbCell} align="right">{row.val4 + "%"}</TableCell>
-                                            <TableCell className = {classes.tbCell} align="right">{row.val5 + "%"}</TableCell>
-                                            <TableCell className = {classes.tbCell} align="right">{row.val6 + "%"}</TableCell>
-                                            <TableCell className = {classes.tbCell} align="right">{row.val7 + "%"}</TableCell>
-                                            <TableCell className = {classes.tbCell} align="right">{row.val8 + "%"}</TableCell>
-                                            <TableCell className = {classes.tbCell} align="right">{row.val9 + "%"}</TableCell>
-                                            <TableCell className = {classes.tbCell} align="right">{row.val10 + "%"}</TableCell>
+                                        <TableRow key={row.val1}>
+                                            <TableCell className={classes.tbCell} align="right"></TableCell>
+                                            <TableCell className={classes.tbCell} align="right">{row.val1 + "%"}</TableCell>
+                                            <TableCell className={classes.tbCell} align="right">{row.val2 + "%"}</TableCell>
+                                            <TableCell className={classes.tbCell} align="right">{row.val3 + "%"}</TableCell>
+                                            <TableCell className={classes.tbCell} align="right">{row.val4 + "%"}</TableCell>
+                                            <TableCell className={classes.tbCell} align="right">{row.val5 + "%"}</TableCell>
+                                            <TableCell className={classes.tbCell} align="right">{row.val6 + "%"}</TableCell>
+                                            <TableCell className={classes.tbCell} align="right">{row.val7 + "%"}</TableCell>
+                                            <TableCell className={classes.tbCell} align="right">{row.val8 + "%"}</TableCell>
+                                            <TableCell className={classes.tbCell} align="right">{row.val9 + "%"}</TableCell>
+                                            <TableCell className={classes.tbCell} align="right">{row.val10 + "%"}</TableCell>
                                         </TableRow>
                                     ))
                                 }
                                 <TableRow>
-                                <TableCell colSpan={11}>
-                                    <div style={{ width: '100%', height: 180 }}>
-                                        <Bar width={400} height={180} data={data} options={options} />
-                                    </div>
-                                </TableCell>
+                                    <TableCell colSpan={11}>
+                                        <div style={{ width: '100%', height: 180 }}>
+                                            <Bar width={400} height={180} data={data} options={options} />
+                                        </div>
+                                    </TableCell>
                                 </TableRow>
                             </TableBody>
 
@@ -219,10 +219,10 @@ export default function ResultTable({ rows, loading, pill, rows2, subgroub }) {
                 </Table>
 
             </TableContainer>
-            <h4 className="graphTitle" style={{marginTop : "28px"}}>AI-predicted HCC risk under ETV treatment</h4>
+            <h4 className="graphTitle" style={{ marginTop: "28px" }}>AI-predicted HCC risk under ETV treatment</h4>
             <hr className="resultLine"></hr>
             <TableContainer component={Paper}>
-                <Table className={classes.table} aria-label="simple table" size = "small">
+                <Table className={classes.table} aria-label="simple table" size="small">
                     {loading ?
                         <div className="tableLoding-rappeer" style={{
                             height: 250
@@ -232,50 +232,78 @@ export default function ResultTable({ rows, loading, pill, rows2, subgroub }) {
                         <>
                             <TableHead>
                                 <TableRow>
-                                    <TableCell className = {classes.tbCell} align="right">Year(s)</TableCell>
-                                    <TableCell className = {classes.tbCell} align="right">1</TableCell>
-                                    <TableCell className = {classes.tbCell} align="right">2</TableCell>
-                                    <TableCell className = {classes.tbCell} align="right">3</TableCell>
-                                    <TableCell className = {classes.tbCell} align="right">4</TableCell>
-                                    <TableCell className = {classes.tbCell} align="right">5</TableCell>
-                                    <TableCell className = {classes.tbCell} align="right">6</TableCell>
-                                    <TableCell className = {classes.tbCell} align="right">7</TableCell>
-                                    <TableCell className = {classes.tbCell} align="right">8</TableCell>
-                                    <TableCell className = {classes.tbCell} align="right">9</TableCell>
-                                    <TableCell className = {classes.tbCell} align="right">10</TableCell>
+                                    <TableCell className={classes.tbCell} align="right">Year(s)</TableCell>
+                                    <TableCell className={classes.tbCell} align="right">1</TableCell>
+                                    <TableCell className={classes.tbCell} align="right">2</TableCell>
+                                    <TableCell className={classes.tbCell} align="right">3</TableCell>
+                                    <TableCell className={classes.tbCell} align="right">4</TableCell>
+                                    <TableCell className={classes.tbCell} align="right">5</TableCell>
+                                    <TableCell className={classes.tbCell} align="right">6</TableCell>
+                                    <TableCell className={classes.tbCell} align="right">7</TableCell>
+                                    <TableCell className={classes.tbCell} align="right">8</TableCell>
+                                    <TableCell className={classes.tbCell} align="right">9</TableCell>
+                                    <TableCell className={classes.tbCell} align="right">10</TableCell>
                                 </TableRow>
                             </TableHead>
                             <TableBody>
                                 {
                                     rows2.map((row) => (
-                                        <TableRow key = {row.val1}>
-                                            <TableCell className = {classes.tbCell} align="right"></TableCell>
-                                            <TableCell className = {classes.tbCell} align="right">{row.val1 + "%"}</TableCell>
-                                            <TableCell className = {classes.tbCell} align="right">{row.val2 + "%"}</TableCell>
-                                            <TableCell className = {classes.tbCell} align="right">{row.val3 + "%"}</TableCell>
-                                            <TableCell className = {classes.tbCell} align="right">{row.val4 + "%"}</TableCell>
-                                            <TableCell className = {classes.tbCell} align="right">{row.val5 + "%"}</TableCell>
-                                            <TableCell className = {classes.tbCell} align="right">{row.val6 + "%"}</TableCell>
-                                            <TableCell className = {classes.tbCell} align="right">{row.val7 + "%"}</TableCell>
-                                            <TableCell className = {classes.tbCell} align="right">{row.val8 + "%"}</TableCell>
-                                            <TableCell className = {classes.tbCell} align="right">{row.val9 + "%"}</TableCell>
-                                            <TableCell className = {classes.tbCell} align="right">{row.val10 + "%"}</TableCell>
+                                        <TableRow key={row.val1}>
+                                            <TableCell className={classes.tbCell} align="right"></TableCell>
+                                            <TableCell className={classes.tbCell} align="right">{row.val1 + "%"}</TableCell>
+                                            <TableCell className={classes.tbCell} align="right">{row.val2 + "%"}</TableCell>
+                                            <TableCell className={classes.tbCell} align="right">{row.val3 + "%"}</TableCell>
+                                            <TableCell className={classes.tbCell} align="right">{row.val4 + "%"}</TableCell>
+                                            <TableCell className={classes.tbCell} align="right">{row.val5 + "%"}</TableCell>
+                                            <TableCell className={classes.tbCell} align="right">{row.val6 + "%"}</TableCell>
+                                            <TableCell className={classes.tbCell} align="right">{row.val7 + "%"}</TableCell>
+                                            <TableCell className={classes.tbCell} align="right">{row.val8 + "%"}</TableCell>
+                                            <TableCell className={classes.tbCell} align="right">{row.val9 + "%"}</TableCell>
+                                            <TableCell className={classes.tbCell} align="right">{row.val10 + "%"}</TableCell>
                                         </TableRow>
                                     ))
                                 }
                                 <TableRow>
-                                <TableCell colSpan={11}>
-                                    <div style={{ width: '100%', height: 180 }}>
-                                        <Bar width={400} height={180} data={data2} options={options2} />
-                                    </div>
-                                </TableCell>
+                                    <TableCell colSpan={11}>
+                                        <div style={{ width: '100%', height: 180 }}>
+                                            <Bar width={400} height={180} data={data2} options={options2} />
+                                        </div>
+                                    </TableCell>
                                 </TableRow>
                             </TableBody>
                         </>}
                 </Table>
             </TableContainer>
-            <p className="nMargin">Subgroup Classification: <b style = {{paddingRight : "20px"}}>{subgroub}</b> </p>
-            <p className="nMargin">Antiviral Recommendation: <b>{pill}</b></p>
+            <hr ></hr>
+            <TableContainer component={Paper}>
+                <Table className={classes.table} aria-label="simple table" size="small">
+                    {loading ?
+                        <div className="tableLoding-rappeer" style={{
+                            height: 250
+                        }}>
+                            <CircularProgress className={classes.Progress} size={50}></CircularProgress>
+                        </div> :
+                        <>
+                            <TableBody>
+
+                                <TableRow>
+                                    <TableCell>
+                                        <div style={{ width: '100%', display: "flex" }}>
+                                        <img src="https://img.icons8.com/external-others-pike-picture/20/000000/external-classification-machine-learning-ai-others-pike-picture.png"/> Subgroup Classification : <b>&nbsp; {subgroub}</b>
+                                        </div>
+                                    </TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>
+                                        <div style={{ width: '100%', display: "flex" }}>
+                                            <img src="https://img.icons8.com/3d-fluency/20/000000/3d-fluency-pill.png" /> Antiviral Recommendation : <b>&nbsp; {pill}</b>
+                                        </div>
+                                    </TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </>}
+                </Table>
+            </TableContainer>
         </div>
     )
 }
